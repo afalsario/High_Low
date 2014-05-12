5cd542acffd353d540b48989042f8c059f2bc3eb
Ashley <a_falsario@yahoo.com>
2014-05-12 13:55:11 -0500

    Revised the "correct guess" code and added an extra elseif statement to code

--- highlow.php
+++ highlow.php
@@ -1,11 +1,11 @@
 <?php
 
 //Computer guesses a random number between 1 and 100
-$number = mt_rand(1, 100);
-$guesses = 0;
+$number = mt_rand(1, 5);
+$guesses = 1;
 
 //Prompt user for first guess
-fwrite(STDOUT, "Guess my number!\n");
+fwrite(STDOUT, "Guess what number I'm thinking of...\n");
 
 //Get input from user
 $user_guess = fgets(STDIN);
@@ -16,16 +16,27 @@ while ($user_guess != $number)
 {
 	if ($user_guess < $number) 
 	{
-		fwrite(STDOUT, "HIGHER...\n");
+		fwrite(STDOUT, "WRONG!!!! Higher...\n");
 	} 
-	elseif ($user_guess > $number) 
+	elseif ($user_guess >= 6)
 	{
-	fwrite(STDOUT, "LOWER...\n");
+		fwrite(STDOUT, "WAY TOO HIGH!! Try again...\n");
+	}
+	else
+	{
+		fwrite(STDOUT, "WRONG!!!! Lower...\n");
 	} 
-
 	$user_guess = fgets(STDIN);
 	$guesses++;
-
+}
+//Output  "correct" statement and 
+//display number of guesses
+if ($guesses == 1) 
+{
+	echo "You got it in 1 try!\n";
+}
+else 
+{
+	echo "You got it in $guesses tries!\n";
 }
 
-echo "You got it in $guesses tries!!\n";
