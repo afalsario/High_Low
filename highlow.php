39db15286124fc46ba7d77b0bd4315055ae510f5
Ashley <a_falsario@yahoo.com>
2014-05-12 11:27:17 -0500

    Basic working game

--- highlow.php
+++ highlow.php
@@ -1,7 +1,7 @@
 <?php
 
 //Computer guesses a random number between 1 and 100
-$number = rand(1, 100);
+$number = rand(1, 5);
 
 //Prompt user for first guess
 fwrite(STDOUT, "Guess my number!\n");
@@ -11,14 +11,18 @@ $user_guess = fgets(STDIN);
 
 //Evaluate
 
-while ($user_guess != $number)  {
-if ($user_guess < $number) {
-	fwrite(STDOUT, "HIGHER...\n");
-	$user_guess = fgets(STDIN);
-} elseif ($user_guess > $number) {
+while ($user_guess != $number)  
+{
+	if ($user_guess < $number) 
+	{
+		fwrite(STDOUT, "HIGHER...\n");
+		$user_guess = fgets(STDIN);
+	} 
+	elseif ($user_guess > $number) 
+	{
 	fwrite(STDOUT, "LOWER...\n");
 	$user_guess = fgets(STDIN);
-} elseif ($user_guess == $number) {
-	fwrite(STDOUT, "You got it!\n");
+	} 
 }
-}
+
+echo "You got it!\n";
